26ef824f8f057306dc67ae8f5ea226d813a9b892
Lucas <lululucas2507@gmail.com>
2026-08-25 16:26:12 -0300

    Código corrigido e adicionado conceito de rect e line

--- curso_pygame/agosto/teste.py
+++ curso_pygame/agosto/teste.py
@@ -3,6 +3,7 @@ pygame.init()
 tela = pygame.display.set_mode((700, 400))
 cor_fundo = (25, 25, 35)
 rodando = True
+BRANCO = (255, 255, 255)
 while rodando:
     for evento in pygame.event.get():
         print(evento)
@@ -12,13 +13,7 @@ while rodando:
             if evento.key == pygame.K_ESCAPE:
                 rodando = False
             if evento.key == pygame.K_SPACE:
-                cor_fundo = (60, 30, 90)
+                pygame.draw.circle(tela, BRANCO, (LARGURA // 2, ALTURA // 2), 8)
     tela.fill(cor_fundo)
-    # Dentro do loop, depois de tela.fill(...)
-    pygame.draw.rect(tela, (60, 180, 90), (0, 330, 800, 120))
-    pygame.draw.rect(tela, (70, 130, 255), (360, 260, 80, 80))
-    pygame.draw.circle(tela, (255, 230, 200), (400, 245), 28)
-    pygame.draw.line(tela, (20, 20, 20), (375, 245), (390, 245), 4)
-    pygame.draw.line(tela, (20, 20, 20), (410, 245), (425, 245), 4)
     pygame.display.flip()
 pygame.quit()

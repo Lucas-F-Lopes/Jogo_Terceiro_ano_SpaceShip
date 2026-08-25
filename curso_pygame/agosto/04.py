4a56ea03c6553c4fc55e4be369c9886e19c55a51
Lucas <lululucas2507@gmail.com>
2026-08-25 16:12:49 -0300

    Adicionando funcionalidades de manter o circulo e criar cenário simples

--- curso_pygame/agosto/04.py
+++ curso_pygame/agosto/04.py
@@ -2,31 +2,21 @@ import pygame
 pygame.init()
 tela = pygame.display.set_mode((700, 400))
 cor_fundo = (25, 25, 35)
-BRANCO = (255, 255, 255)
 rodando = True
-mostrar_circulo = False
-
 while rodando:
-
-    tela.fill(cor_fundo)
-
     for evento in pygame.event.get():
         print(evento)
-
         if evento.type == pygame.QUIT:
             rodando = False
-
         if evento.type == pygame.KEYDOWN:
-
             if evento.key == pygame.K_ESCAPE:
                 rodando = False
-
-            if evento.key == pygame.K_a:
-                mostrar_circulo = True
-
-    if mostrar_circulo:
-        pygame.draw.circle(tela, BRANCO, (350, 200), 20)
-
+            if evento.key == pygame.K_SPACE:
+                cor_fundo = (60, 30, 90)
+    tela.fill(cor_fundo)
+    pygame.draw.rect(tela, (60, 180, 90), (0, 330, 800, 120))
+    pygame.draw.circle(tela, (255, 230, 200), (400, 245), 28)
+    pygame.draw.line(tela, (20, 20, 20), (375, 245), (390, 245), 4)
+    pygame.draw.line(tela, (20, 20, 20), (410, 245), (425, 245), 4)
     pygame.display.flip()
-
 pygame.quit()
